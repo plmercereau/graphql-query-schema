a5d214f61de3581526eeba7c52f68691c7b5c55c
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-01-07 23:44:50 +0100

    chore: cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,15 @@
-import fs from 'fs'
-import path from 'path'
-
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-const PWD = process.env.PWD
-const pkg = require(path.join(PWD, 'package.json'))
-
-const tsEntry = path.resolve(PWD, 'src/index.ts')
-const entry = fs.existsSync(tsEntry) ? tsEntry : tsEntry.replace('.ts', '.tsx')
-
-const deps = [
-    ...Object.keys(Object.assign({}, pkg.peerDependencies, pkg.dependencies))
-]
-
 export default defineConfig({
-    plugins: [
-        dts({
-            exclude: ['**/*.spec.ts', '**/*.test.ts', '**/tests/**'],
-            entryRoot: 'src',
-            skipDiagnostics: true
-        })
-    ],
+    plugins: [dts()],
     test: {
         dir: 'tests'
     },
     build: {
         sourcemap: true,
         lib: {
-            entry,
-            name: pkg.name,
+            entry: 'src/index.ts',
             fileName: format =>
                 format === 'cjs' ? `index.cjs.js` : `index.esm.js`,
             formats: ['cjs', 'es']
