6c93a8958b498b5cae2aca575849228a852f3481
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-01-25 13:59:03 +0100

    clean

--- play/index.ts
+++ play/index.ts
@@ -1,5 +1,7 @@
 import { fetchClient } from '../src'
+
 import schema from '../schemas/hasura'
+
 const client = fetchClient({
   schema,
   url: 'http://localhost:1337/v1/graphql',
@@ -15,21 +17,24 @@ const main = async () => {
     select: { email: true, id: true, createdAt: true },
     variables: { where: { email: { _eq: email } } }
   })
+
   if (!user) {
     user = await client.mutation.insertUser({
       variables: { object: { locale: 'en', email } },
       select: { email: true, id: true, createdAt: true }
     })
   }
+
   console.log('User', user)
+
   const todos = await client.query.todos({ select: { user: true, contents: true } })
   console.log(`${todos.length} todos`)
 
-  const todo = await client.mutation.insertTodo({
+  const newTodo = await client.mutation.insertTodo({
     variables: { object: { contents: 'encore', userId: user.id } },
-    select: { contents: true }
+    select: { contents: true, createdAt: true }
   })
-  console.log('Inserted todo', todo.contents)
+  console.log('Inserted todo', newTodo.createdAt)
 }
 
 main()
